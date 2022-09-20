7cfc572f0667d11f14a72522383bcd94d0416d3b
kelvinpraises <kelvinpraises@gmail.com>
2022-09-20 07:12:22 +0100

    Initialized Repo

---
+++ src/components/Scaffold.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import Navigation from "./Navigation";
+import styled from "styled-components";
+import AuthModal from "./Modal";
+import AddContactModal from "./AddContactModal";
+
+interface IScaffoldProp {
+  children: React.ReactNode;
+}
+
+const SScaffold = styled.div`
+  display: flex;
+`;
+
+const SContainer = styled.div`
+  width: 100%;
+`;
+
+const Scaffold: React.FC<IScaffoldProp> = ({ children }) => {
+  return (
+    <SScaffold>
+      <Navigation />
+      <SContainer>{children}</SContainer>
+      <AuthModal />
+      <AddContactModal />
+    </SScaffold>
+  );
+};
+
+export default Scaffold;
